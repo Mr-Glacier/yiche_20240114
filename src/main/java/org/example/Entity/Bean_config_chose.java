90ef41f4ab9dd34bd999ed4ba47fb126491e946f
Mr-Glacier <K15100415112@163.com>
2024-01-18 13:57:01 +0800

    提交

---
+++ src/main/java/org/example/Entity/Bean_config_chose.java
@@ -0,0 +1,86 @@
+package org.example.Entity;
+
+public class Bean_config_chose {
+    private int C_ID;
+    private String C_congig_type;
+
+    public String getC_congig_type() {
+        return C_congig_type;
+    }
+
+    public void setC_congig_type(String c_congig_type) {
+        C_congig_type = c_congig_type;
+    }
+
+    private String C_version_id;
+    private String C_congig_name;
+    private String C_config_value;
+    private String C_config_price;
+    private String C_config_desc;
+    private String C_config_describe;
+    private String C_source;
+
+    public String getC_source() {
+        return C_source;
+    }
+
+    public void setC_source(String c_source) {
+        C_source = c_source;
+    }
+
+    public int getC_ID() {
+        return C_ID;
+    }
+
+    public void setC_ID(int c_ID) {
+        C_ID = c_ID;
+    }
+
+    public String getC_version_id() {
+        return C_version_id;
+    }
+
+    public void setC_version_id(String c_version_id) {
+        C_version_id = c_version_id;
+    }
+
+    public String getC_congig_name() {
+        return C_congig_name;
+    }
+
+    public void setC_congig_name(String c_congig_name) {
+        C_congig_name = c_congig_name;
+    }
+
+    public String getC_config_value() {
+        return C_config_value;
+    }
+
+    public void setC_config_value(String c_config_value) {
+        C_config_value = c_config_value;
+    }
+
+    public String getC_config_price() {
+        return C_config_price;
+    }
+
+    public void setC_config_price(String c_config_price) {
+        C_config_price = c_config_price;
+    }
+
+    public String getC_config_desc() {
+        return C_config_desc;
+    }
+
+    public void setC_config_desc(String c_config_desc) {
+        C_config_desc = c_config_desc;
+    }
+
+    public String getC_config_describe() {
+        return C_config_describe;
+    }
+
+    public void setC_config_describe(String c_config_describe) {
+        C_config_describe = c_config_describe;
+    }
+}
